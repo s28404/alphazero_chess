e8042bd6cee083a971b1539efc0eed9180a093da
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2025-10-04 11:14:27 +0200

    .

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ done = False
 
 while not done:
     action = random.sample(env.legal_moves)
+    action_space = env.legal_moves
+    observation_space = env.board.fen()
+    print(f"Action Space: {action_space}")
+    print(f"Observation Space: {observation_space}")
     env.step(action)
     print(env.render(mode='unicode'))
 
